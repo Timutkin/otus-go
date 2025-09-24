eea5568596a4ee303de6e9608f38d2b458777722
Timofey Utkin <90320520+Timutkin@users.noreply.github.com>
2025-09-24 18:27:41 +0300

    hw 6 is dome

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,33 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	for _, stage := range stages {
+		in = stage(in)
+		out := make(Bi)
+		go func(in In) {
+			defer func() {
+				close(out)
+				for v := range in {
+					_ = v
+				}
+			}()
+			for {
+				select {
+				case <-done:
+					return
+				case v, ok := <-in:
+					if !ok {
+						return
+					}
+					select {
+					case <-done:
+						return
+					case out <- v:
+					}
+				}
+			}
+		}(in)
+		in = out
+	}
+	return in
 }
