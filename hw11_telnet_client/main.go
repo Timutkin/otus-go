b03abb2f2f49784abcecc1a4cc5bb102af3bc052
Timofey Utkin <xXwild.duckXx@yandex.ru>
2025-10-30 20:52:27 +0300

    hw 11 is done

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,55 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+var timeout time.Duration
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.DurationVar(&timeout, "timeout", DefaultTimeoutSecond, "connection timeout")
+	flag.Parse()
+
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+
+	address := net.JoinHostPort(host, port)
+	client := NewTelnetClient(address, timeout, os.Stdin, os.Stdout)
+	err := client.Connect()
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "failed to connect to %s : %v\n", address, err)
+		os.Exit(1)
+	}
+	defer client.Close()
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := client.Send(); err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "send error: %v\n", err)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := client.Receive(); err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "receive error: %v\n", err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	wg.Wait()
 }
