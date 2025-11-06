456e305a9dee0bbfed175d208742868ad546d5cc
Timofey Utkin <xXwild.duckXx@yandex.ru>
2025-11-06 18:44:03 +0300

    fixes modules

--- hw12_13_14_15_16_calendar/cmd/calendar/main.go
+++ hw12_13_14_15_16_calendar/cmd/calendar/main.go
@@ -8,10 +8,10 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/app"
-	"github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/logger"
-	internalhttp "github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/server/http"
-	memorystorage "github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/storage/memory"
+	"github.com/timutkin/otus-go/hw12_13_14_15_calendar/internal/app"
+	"github.com/timutkin/otus-go/hw12_13_14_15_calendar/internal/logger"
+	internalhttp "github.com/timutkin/otus-go/hw12_13_14_15_calendar/internal/server/http"
+	memorystorage "github.com/timutkin/otus-go/hw12_13_14_15_calendar/internal/storage/memory"
 )
 
 var configFile string
